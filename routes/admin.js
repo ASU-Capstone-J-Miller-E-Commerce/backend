0808310b0b95f073515574632f5dcb4e3fd66641
derekcdee <dcdailey@asu.edu>
2025-03-01 12:18:48 -0700

    removed array of array

--- routes/admin.js
+++ routes/admin.js
@@ -21,7 +21,7 @@ router.get('/users', async (req, res) =>
 {
     try{
         const users = await User.find();
-        res.status(200).json(makeResponse('success', [users], ['Fetched all users from database.'], false));
+        res.status(200).json(makeResponse('success', users, ['Fetched all users from database.'], false));
     }catch(ex)
     {
         console.error(ex);
