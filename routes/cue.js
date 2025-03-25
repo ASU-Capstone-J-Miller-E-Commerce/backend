4fb8283a3bf5771fc93e749213658050f4db7409
Ethan Keyser <ethan.keyser2004@gmail.com>
2025-03-24 21:04:24 -0700

    Updated Cue Fields

--- routes/cue.js
+++ routes/cue.js
@@ -27,31 +27,7 @@ router.get('/:id', getCue, (req, res, next) => {
 })
 
 router.post('admin/', authAdmin, async (req, res, next) => {
-    const cue = new Cue({
-        prodId: req.body.prodId,
-        listDate: req.body.listDate,
-        availableDate: req.body.availableDate,
-        price: req.body.price,
-        tipMaterial: req.body.tipMaterial,
-        tipSize: req.body.tipSize,
-        ferruleMaterial: req.body.ferruleMaterial,
-        shaftMaterial: req.body.shaftMaterial,
-        collarMaterial: req.body.collarMaterial,
-        jointPinSize: req.body.jointPinSize,
-        jointPinMaterial: req.body.jointPinMaterial,
-        jointCollarMaterial: req.body.jointCollarMaterial,
-        forearmSize: req.body.forearmSize,
-        forearmMaterial: req.body.forearmMaterial,
-        forearmPointMaterial: req.body.forearmPointMaterial,
-        veneerMatieral: req.body.veneerMatieral,
-        handleMaterial: req.body.handleMaterial,
-        buttSleeveSize: req.body.buttSleeveSize,
-        buttSleeveMaterial: req.body.buttSleeveMaterial,
-        buttSleeveVeneerMaterial: req.body.buttSleeveVeneerMaterial,
-        buttCapMaterial: req.body.buttCapMaterial,
-        bumperMaterial: req.body.bumperMaterial
-    })
-
+    const cue = new Cue(req.body);
     try {
         const newCue = await cue.save()
         
@@ -62,99 +38,16 @@ router.post('admin/', authAdmin, async (req, res, next) => {
 })
 
 router.patch('admin/:id', authAdmin, getCue, async (req, res, next) => {
-    if(req.body.prodId != null)
-    {
-        res.cue.prodId = req.body.prodId
-    }
-    if(req.body.listDate != null)
-    {
-        res.cue.listDate = req.body.listDate
-    }
-    if(req.body.availableDate != null)
-    {
-        res.cue.availableDate = req.body.availableDate
-    }
-    if(req.body.price != null)
-    {
-        res.cue.price = req.body.price
-    }
-    if(req.body.tipMaterial != null)
-    {
-        res.cue.tipMaterial = req.body.tipMaterial
-    }
-    if(req.body.tipSize != null)
-    {
-        res.cue.tipSize = req.body.tipSize
-    }
-    if(req.body.ferruleMaterial != null)
-    {
-        res.cue.ferruleMaterial = req.body.ferruleMaterial
-    }
-    if(req.body.shaftMaterial != null)
-    {
-        res.cue.shaftMaterial = req.body.shaftMaterial
-    }
-    if(req.body.collarMaterial != null)
-    {
-        res.cue.collarMaterial = req.body.collarMaterial
-    }
-    if(req.body.jointPinSize != null)
-    {
-        res.cue.jointPinSize = req.body.jointPinSize
-    }
-    if(req.body.jointPinMaterial != null)
-    {
-        res.cue.jointPinMaterial = req.body.jointPinMaterial
-    }
-    if(req.body.jointCollarMaterial != null)
-    {
-        res.cue.jointCollarMaterial = req.body.jointCollarMaterial
-    }
-    if(req.body.forearmSize != null)
-    {
-        res.cue.forearmSize = req.body.forearmSize
-    }
-    if(req.body.forearmMaterial != null)
-    {
-        res.cue.forearmMaterial = req.body.forearmMaterial
-    }
-    if(req.body.forearmPointMaterial != null)
-    {
-        res.cue.forearmPointMaterial = req.body.forearmPointMaterial
-    }
-    if(req.body.veneerMatieral != null)
-    {
-        res.cue.veneerMatieral = req.body.veneerMatieral
-    }
-    if(req.body.handleMaterial != null)
-    {
-        res.cue.handleMaterial = req.body.handleMaterial
-    }
-    if(req.body.buttSleeveSize != null)
-    {
-        res.cue.buttSleeveSize = req.body.buttSleeveSize
-    }
-    if(req.body.buttSleeveMaterial != null)
-    {
-        res.cue.buttSleeveMaterial = req.body.buttSleeveMaterial
-    }
-    if(req.body.buttSleeveVeneerMaterial != null)
-    {
-        res.cue.buttSleeveVeneerMaterial = req.body.buttSleeveVeneerMaterial
-    }
-    if(req.body.buttCapMaterial != null)
-    {
-        res.cue.buttCapMaterial = req.body.buttCapMaterial
-    }
-    if(req.body.bumperMaterial != null)
-    {
-        res.cue.bumperMaterial = req.body.bumperMaterial
-    }
-    
-    res.cue.updatedOn = Date.now()
-
-
     try {
+
+        for (const key in req.body) {
+            if (req.body[key] != null) {
+                res.cue[key] = req.body[key];
+            }
+        }
+
+        res.cue.updatedOn = Date.now();
+
         const updateCue = await res.cue.save()
 
         res.json(makeResponse('success', [updateCue], ['updated a cue in the database'], false))
