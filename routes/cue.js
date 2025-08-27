65178a296d2baa6aeff6621eeb40c2331102070a
Derek Dailey <dcdailey@asu.edu>
2025-08-27 09:46:47 -0700

    Material dereffing cue page

--- routes/cue.js
+++ routes/cue.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const Cue = require('../models/cue')
+const Wood = require('../models/wood')
+const Crystal = require('../models/crystal')
 const { makeError, makeData } = require('../response/makeResponse');
 const router = express.Router()
 const { authUser } = require('./authorization')
@@ -23,9 +25,14 @@ router.get('/', async (req, res, next) => {
     }
 })
 
-//get one by guid
-router.get('/:guid', getCueByGuid, (req, res, next) => {
-    res.status(200).json(makeData(res.cue))
+//get one by guid with dereferenced materials
+router.get('/:guid', getCueByGuid, async (req, res, next) => {
+    try {
+        const cueWithMaterials = await dereferenceMaterials(res.cue);
+        res.status(200).json(makeData(cueWithMaterials));
+    } catch (err) {
+        res.status(500).json(makeError([err.message]));
+    }
 })
 
 //get one by id (for admin)
@@ -61,6 +68,60 @@ async function getCue(req, res, next) {
 
     res.cue = cue
     next()
+}
+
+async function dereferenceMaterials(cue) {
+    const cueObj = cue.toObject();
+    
+    // List of material fields that might contain GUIDs
+    const materialFields = [
+        'forearmInlayMaterial',
+        'forearmPointInlayMaterial', 
+        'handleInlayMaterial',
+        'buttSleeveInlayMaterial',
+        'buttSleevePointInlayMaterial',
+        'shaftMaterial',
+        'ferruleMaterial',
+        'jointPinMaterial',
+        'jointCollarMaterial',
+        'buttCapMaterial',
+        'forearmMaterial',
+        'handleMaterial',
+        'buttSleeveMaterial'
+    ];
+    
+    // Process each material field
+    for (const field of materialFields) {
+        const materialValue = cueObj[field];
+        
+        if (materialValue && typeof materialValue === 'string') {
+            try {
+                // Try to find as wood first
+                let material = await Wood.findOne({ guid: materialValue })
+                    .select('guid commonName')
+                    .lean();
+                
+                if (material) {
+                    cueObj[field] = material;
+                    continue;
+                }
+                
+                // If not found as wood, try crystal
+                material = await Crystal.findOne({ guid: materialValue })
+                    .select('guid crystalName')
+                    .lean();
+                
+                if (material) {
+                    cueObj[field] = material;
+                }
+                
+            } catch (err) {
+                console.error(`Error dereferencing material ${materialValue} for field ${field}:`, err);
+            }
+        }
+    }
+    
+    return cueObj;
 }
 
 module.exports = router
