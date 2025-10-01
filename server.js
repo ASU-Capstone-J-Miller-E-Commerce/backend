ea61f7f652d5a56f9698f0cb8b00c42dbaba79cc
Ethan Keyser <87833532+ethankeyser@users.noreply.github.com>
2025-10-01 14:24:55 -0700

    Changed ip

--- server.js
+++ server.js
@@ -121,6 +121,6 @@ app.get('/', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "127.0.0.1", () => {
   console.log(`Server running on port ${PORT}`);
 });
